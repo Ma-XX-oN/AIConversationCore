1f0ef40db1a18388ccc15a1488d7945e890e14e6
Ma-XX-oN <Ma-XX-oN@users.noreply.github.com>
2026-09-05 16:11:36 -0400

    api: export canonical render contract

--- src/index.js
+++ src/index.js
@@ -12,6 +12,20 @@ export {
 } from './projections/turn-header.js';
 
 export { renderCanonicalMarkdown } from './projections/markdown.js';
+export {
+  markdownToHtml,
+  renderCanonicalHtml,
+  renderCanonicalStylesheet
+} from './projections/html.js';
+export {
+  PRESENTATION_SCHEMA_VERSION,
+  buildCanonicalPresentation
+} from './projections/presentation.js';
+export {
+  RENDER_FORMATS,
+  RENDER_INPUT_KINDS,
+  renderConversation
+} from './render.js';
 
 export { deriveTurns } from './derive/turns.js';
 export { adaptChatGPTRecords } from './adapters/chatgpt.js';
