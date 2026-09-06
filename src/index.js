d0ab1501a87adcbb8b93f3d2fe5839929579433a
Ma-XX-oN <Ma-XX-oN@users.noreply.github.com>
2026-09-05 20:29:50 -0400

    export canonical presentation builder

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ export {
 } from './projections/turn-header.js';
 
 export { renderCanonicalMarkdown } from './projections/markdown.js';
+export { buildCanonicalPresentation } from './projections/presentation.js';
 export { projectCanonicalConversation } from './projections/structured.js';
 
 export { deriveTurns } from './derive/turns.js';
