2e0507a2ec235a5dc4fe91e1c6164d6324b816d8
Ma-XX-oN <Ma-XX-oN@users.noreply.github.com>
2026-09-06 16:24:48 -0400

    feat: expose revision-aware presentation labels

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ export {
 } from './projections/turn-header.js';
 
 export { renderCanonicalMarkdown } from './projections/markdown.js';
-export { buildCanonicalPresentation } from './projections/presentation.js';
+export { buildCanonicalPresentation } from './projections/presentation-revisions.js';
 export { projectCanonicalConversation } from './projections/structured.js';
 
 export { deriveTurns } from './derive/turns.js';
