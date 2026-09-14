cf34d9374f51ac525acfb90cfd6b247006a7bf6e
Ma-XX-oN <Ma-XX-oN@users.noreply.github.com>
2026-09-14 15:41:21 -0400

    build: finalize DownloadConversation Core line at 1.0.0

--- dist/aiconversationcore.chatgpt.browser.js
+++ dist/aiconversationcore.chatgpt.browser.js
@@ -101,7 +101,7 @@ if(__exports != exports)module.exports = exports;return module.exports}));
 (function bootstrapAIConversationCore(global) {
   'use strict';
 
-const VERSION = "1.0.0-issue.98.1";
+const VERSION = "1.0.0";
 
 /**
  * Returns the authoritative AIConversationCore version.
