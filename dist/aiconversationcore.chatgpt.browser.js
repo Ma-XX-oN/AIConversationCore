259376c1f16a3c67cc13ab05cc5f50c22fe3f060
Ma-XX-oN <Ma-XX-oN@users.noreply.github.com>
2026-09-25 14:33:11 -0400

    release: prepare v1.0.0

--- dist/aiconversationcore.chatgpt.browser.js
+++ dist/aiconversationcore.chatgpt.browser.js
@@ -101,7 +101,7 @@ if(__exports != exports)module.exports = exports;return module.exports}));
 (function bootstrapAIConversationCore(global) {
   'use strict';
 
-const VERSION = "1.0.0-issue.106.20";
+const VERSION = "1.0.0";
 
 /**
  * Returns the authoritative AIConversationCore version.
