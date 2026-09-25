b320215a8d98bea5195c14921caaf3a59eaae7ee
Ma-XX-oN <Ma-XX-oN@users.noreply.github.com>
2026-09-25 01:41:22 -0400

    ci(#106): validate Group 2 with a fresh result iteration

--- dist/aiconversationcore.chatgpt.browser.js
+++ dist/aiconversationcore.chatgpt.browser.js
@@ -10,7 +10,7 @@
 (function bootstrapAIConversationCore(global) {
   'use strict';
 
-const VERSION = "1.0.0-issue.106.7";
+const VERSION = "1.0.0-issue.106.8";
 
 /**
  * Returns the authoritative AIConversationCore version.
